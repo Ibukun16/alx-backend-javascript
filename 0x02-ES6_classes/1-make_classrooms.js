26005b0cd368ffddaed41767f7a40360aeffa759
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-05 09:51:07 +0100

    Update the task 1 to 3 in ES6 classes to return result

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,6 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const classroomSizes = [
-    new ClassRoom(19),
-    new ClassRoom(20),
-    new ClassRoom(34),
-  ];
-  return classroomSizes;
+  const classroomSizes = [19, 20, 34];
+  return classroomSizes.map((size) => new ClassRoom(size));
 }
