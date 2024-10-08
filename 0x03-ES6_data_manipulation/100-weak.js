14fbc59ffb6afd6c24357f113677f2d759117501
Ibukun16 <ib.agunbiade18@gmail.com>
2024-10-08 22:05:48 +0100

    Update all changes accordingly to give correction

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,10 +2,10 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   if (
-    typeof endpoint !== 'object'
-    || endpoint === null
-    || !endpoint.protocol
-    || !endpoint.name
+    typeof endpoint !== 'object' ||
+    endpoint === null ||
+    !endpoint.protocol ||
+    !endpoint.name
   ) {
     throw new Error('Invalid endpoint');
   }
@@ -17,9 +17,9 @@ export function queryAPI(endpoint) {
   const load = weakMap.get(endpoint);
 
   weakMap.set(endpoint, load + 1);
-  if (load >= 5) {
+  if (load + 1 >= 5) {
     throw new Error('Endpoint load is high');
   }
 
-  return load;
+  return load + 1;
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,20 +1,16 @@
 export default function cleanSet(set, startString) {
   if (
-    !set ||
-    !startString ||
-    !(set instanceof Set) ||
-    typeof startString !== 'string'
+    !set
+    || !startString
+    || !(set instanceof Set)
+    || typeof startString !== 'string'
   ) {
     return '';
   }
 
   const result = Array.from(set)
-    .filter((value) =>
-      value !== undefined ? value.startsWith(startString) : '',
-    )
-    .map((value) =>
-      value !== undefined ? value.slice(startString.length) : '',
-    );
+    .filter((value) => (value !== undefined ? value.startsWith(startString) : ''))
+    .map((value) => (value !== undefined ? value.slice(startString.length) : ''));
 
   return result.join('-');
 }
