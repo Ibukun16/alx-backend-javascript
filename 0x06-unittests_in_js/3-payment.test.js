cb809540cb5fb0a783a3fe7e9f25cf97c7711734
Ibukun16 <ib.agunbiade18@gmail.com>
2024-12-01 08:27:57 +0100

    Update changes to file 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,11 +5,15 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 describe('sendPaymentRequestToApi', () => {
   it('sendPaymentRequestToApi uses the calculateNumber method of Utils', () => {
-    const bigBrother = sinon.spy(Utils);
+    const spyUtils = sinon.spy(Utils);
+    const spyConsole = sinon.spy(console);
 
     sendPaymentRequestToApi(100, 20);
-    expect(bigBrother.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
-    expect(bigBrother.calculateNumber.callCount).to.be.equal(1);
-    bigBrother.calculateNumber.restore();
+    expect(spyUtils.calculateNumber.calledOnceWithExactly('SUM', 100, 20)).to.be
+      .true;
+    expect(spyConsole.log.calledOnceWithExactly('The total is: 120')).to.be
+      .true;
+    spyUtils.calculateNumber.restore();
+    spyConsole.log.restore();
   });
 });
