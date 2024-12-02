097f56554728ce9fa19305f7844c1388e575a10f
Ibukun16 <ib.agunbiade18@gmail.com>
2024-12-02 08:59:31 +0100

    Update changes and return correct result

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -6,44 +6,42 @@ const fs = require('fs');
  * @author Ibukun Agunbiade <https://github.com/Ibukun16>
  */
 
-const countStudents = (fileName) =>
-  new Promise((resolve, reject) => {
-    fs.readFile(fileName, { encoding: 'utf-8' }, (error, data) => {
-      if (error) {
-        reject(new Error('Cannot load the database'));
-      }
-      if (data) {
-        const lines = data.toString('utf-8').trim().split('\n');
-        const students = {};
-        const fields = {};
-        const header = lines[0].split(',');
-        const dataBody = lines.slice(1);
-        const studentNames = header.slice(0, header.length - 1);
+const countStudents = (fileName) => new Promise((resolve, reject) => {
+  fs.readFile(fileName, { encoding: 'utf-8' }, (error, data) => {
+    if (error) {
+      reject(new Error('Cannot load the database'));
+    }
+    if (data) {
+      const lines = data.toString('utf-8').trim().split('\n');
+      const students = {};
+      const header = lines[0].split(',');
+      const dataBody = lines.slice(1);
+      const studentNames = header.slice(0, header.length - 1);
 
-        for (const eachRow of dataBody) {
-          const studentRec = eachRow.split(',');
-          const studentValues = studentRec.slice(0, studentRec.length - 1);
-          const field = studentRec[studentRec.length - 1];
-          if (!Object.keys(students).includes(field)) {
-            students[field] = [];
-          }
-          const studentEntry = studentNames.map((name, idx) => [
-            name,
-            studentValues[idx],
-          ]);
-          students[field].push(Object.fromEntries(studentEntry));
+      for (const eachRow of dataBody) {
+        const studentRec = eachRow.split(',');
+        const studentValues = studentRec.slice(0, studentRec.length - 1);
+        const field = studentRec[studentRec.length - 1];
+        if (!Object.keys(students).includes(field)) {
+          students[field] = [];
         }
-
-        console.log(`Number of students: ${dataBody.length}`);
-        for (const [key, val] of Object.entries(students)) {
-          const category = val.map((student) => student.firstname).join(', ');
-          console.log(
-            `Number of students in ${key}: ${val.length}. List: ${category}`,
-          );
-        }
-        resolve(data);
+        const studentEntry = studentNames.map((name, idx) => [
+          name,
+          studentValues[idx],
+        ]);
+        students[field].push(Object.fromEntries(studentEntry));
       }
-    });
+
+      console.log(`Number of students: ${dataBody.length}`);
+      for (const [key, val] of Object.entries(students)) {
+        const category = val.map((student) => student.firstname).join(', ');
+        console.log(
+          `Number of students in ${key}: ${val.length}. List: ${category}`,
+        );
+      }
+      resolve(data);
+    }
   });
+});
 
 module.exports = countStudents;
